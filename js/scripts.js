0621b58255e38b59d8145e9657c4eaba671cc147
Ben Stoller <stollersystemsound@gmail.com>
2020-08-18 12:07:39 -0700

    add function with for loop
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 //Business Logic
 function handleInput (countTo,countBy) {
-  if (!countTo || !countBy || countTo < 0 || countBy < 0 || isNaN(countBy) || isNaN(countTo) || countBy > countTo){
+  if (!countTo || !countBy || countTo < 0 || countBy < 0 || isNaN(countBy) || isNaN(countTo) || parseInt(countBy) > parseInt(countTo)){ 
     alert (" Please make sure you are entering a valid number into both fields. Thanks!")
   } else{
-
+    console.log("pass!")
+    countUp(countTo,countBy);
   }
+}
+
+function countUp (countTo,countBy) {
+  console.log(countTo,countBy)
+  let countedArray = [0];
+  for (let index = 0; index < countTo; index += parseInt(countBy)) {
+    countedArray.push(countedArray[index] + parseInt(countBy));
+  }
+  countedArray;
+  $("#result").append(countedArray);
 }
 
 
@@ -18,5 +29,6 @@ window.onload = function() {
     let countBy = $("#countBy").val();
     handleInput(countTo,countBy)
     console.log(countTo,countBy)
+    $("#result").show();
   });  
 };
